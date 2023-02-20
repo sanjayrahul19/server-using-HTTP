1b571828a3c266b495406c84e01a0b0788ddeaaa
sanjayrahul19 <88281258+sanjayrahul19@users.noreply.github.com>
2023-02-20 11:50:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer((req, res) => {
+  //   fs.writeFile("demo.txt", "hello", function (err) {
+  //     if (err) {
+  //       return console.log(err);
+  //     }
+  //     console.log("Success");
+  //   }); //to create and write file
+
+  //   fs.readFile("demo.txt", (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       res.write(data);
+  //       res.end();
+  //     }
+  //   }); //to read file
+
+  fs.appendFile("demo.txt", "sanjay", (err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("Successfully Appended");
+    }
+  }); //to append text
+
+  // fs.rename("demo.txt", "renamed.txt", (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Sucessfully Renamed");
+  //   }
+  // });
+
+  // fs.unlink("demo.txt", (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Successfully Deleted");
+  //   }
+  // }); // to delete a file
+
+  //   fs.readFile("index.html", (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       res.write(data);
+  //       res.end();
+  //     }
+  //   }); //to read and display index.html page
+});
+
+server.listen(PORT, () => {
+  console.log("Server is up and running");
+});
